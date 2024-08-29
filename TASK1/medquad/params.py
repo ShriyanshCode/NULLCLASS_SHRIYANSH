c75abe37cfc2b7f2b6a505e6e7a36d69b56034f3
ShriyanshCode <146875550+ShriyanshCode@users.noreply.github.com>
2024-08-29 17:57:10 +0530

    Update params.py

--- TASK1/medquad/params.py
+++ TASK1/medquad/params.py
@@ -1,5 +1,5 @@
 #Replace with own token, all are open source tokens. Can use HF_TOKEN for other models(Llama 3, Mistral etc.) via HuggingFace.
 #Code will have to modified for HF models as done on Shriyansh_MEDquad_colab.ipynb
-MONGO_URI = "mongodb+srv://shriyanshpatnaik2022:Ks34%40%232a@cluster0.awlceyw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-GOOGLE_API_KEY="AIzaSyAs74Na4sxNAQufscHZLG1Y1pCmegkuN-Q"
-HF_TOKEN="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"
+MONGO_URI = ""
+GOOGLE_API_KEY=""
+HF_TOKEN=""
